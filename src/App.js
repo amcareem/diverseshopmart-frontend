64f9a151b5547ab551aab19e35b162e0ea14b1db
Ahamed Musthafa Careem <68745704+amcareem@users.noreply.github.com>
2023-12-03 13:16:36 +0530

    Basic Registration Form setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import RegistrationForm from './RegistrationForm'; // Import the RegistrationForm component
+import logo from './logo.svg'; // Assuming you have this logo file in your src folder
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Route for Registration Form */}
+          <Route path="/register" element={<RegistrationForm />} />
+
+          {/* Default Route */}
+          <Route path="/" element={
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>Edit <code>src/App.js</code> and save to reload.</p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+          } />
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
 export default App;
